be54e3341a367ca1ae7837f1865c5370626c114f
skody11 <sreemanthk11@gmail.com>
2018-10-02 23:38:45 +0100

    Initial commit
    Signed-off-by: skody11 <sreemanthk11@gmail.com>

---
+++ src/main/scala/org/ssl/Prog1.scala
@@ -0,0 +1,9 @@
+package org.ssl
+
+object Prog1 {
+ def main(args: Array[String]): Unit ={
+   println("Scala 99 puzzle prog - 1")
+   val l = List(1,2,3,4,5,6,7,8)
+   println(s"Last element: ${l.last}")
+  }
+}
