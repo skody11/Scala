6c5d2c751359a74b025da170c0b2dc8e49c7c503
skody11 <sreemanthk11@gmail.com>
2018-10-04 22:22:44 +0100

    Prog 7

--- src/main/scala/org/ssl/Prog7.scala
+++ src/main/scala/org/ssl/Prog7.scala
@@ -4,7 +4,14 @@ object Prog7 {
 
   def main(args: Array[String]): Unit ={
     val l = List(List(List(1, 1), 2, List(3, List(5, 8))))
-    println(l.flatten)
+    println(flat(l))
   }
 
+  def flat[A] (li: List[Any]): List[Any] = {
+
+    li flatMap  {
+      case i: List[_] => flat(i)
+      case e => List(e)
+    }
+  }
 }
