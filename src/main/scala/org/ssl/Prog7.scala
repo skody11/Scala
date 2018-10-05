bee12c13235a4abf89976b5cb39bfdb717a2a695
Sreemanth Koduru <sreemanth.koduru@skybettingandgaming.com>
2018-10-05 13:46:59 +0100

    Initial commit

--- src/main/scala/org/ssl/Prog7.scala
+++ src/main/scala/org/ssl/Prog7.scala
@@ -5,6 +5,19 @@ object Prog7 {
   def main(args: Array[String]): Unit ={
     val l = List(List(List(1, 1), 2, List(3, List(5, 8))))
     println(l.flatten)
+    print(factorial(5))
   }
 
+    def factorial(i:Int): Int ={
+      if (i > 0)
+        {
+          println(s"In if=$i")
+          i * factorial(i - 1)
+        }
+      else
+        {
+          println(s"In else=$i")
+          1
+        }
+    }
 }
